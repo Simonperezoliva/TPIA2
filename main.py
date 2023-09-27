8925c30e90289747670afd91fe2c3c26d7e89a1f
Simonperezoliva <69171330+Simonperezoliva@users.noreply.github.com>
2023-09-27 14:20:32 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-#from matplotlib.widgets import Button #widget para agregar botones a la gráfica de matplotlib
-#from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-#from matplotlib.figure import Figure
 #import math
 from tkinter import *
-from tkinter import filedialog
 from tkinter.filedialog import askopenfilename
-from tkinter import ttk
-import os
 
 def calcularKMeans():
     k = int(kmField.get())
-    #clases = np.array(k)
-
-    # Se lee el dataset
-    data = pd.DataFrame()
-    data = pd.read_csv(v.get(), sep= ';', decimal=",") #sep es para indicar mediante qué se separa cada registro del dataset. En este caso se usa ;
-    data = data.assign(centroide='NaN') #se agrega columna centroide para verificar a qué centroide pertenece cada punto
-    centroides = np.array(data.sample(n = k)) #se inicializan los k centroides aleatorios
-    print(centroides)
-    df_centroides = pd.DataFrame(centroides)
-    graficarData(data, df_centroides)
+    if k > 1  and k < 6:
+        # Se lee el dataset
+        data = pd.DataFrame()
+        data = pd.read_csv(v.get(), sep= ';', decimal=",") #sep es para indicar mediante qué se separa cada registro del dataset. En este caso se usa ;
+        data = data.assign(centroide='NaN') #se agrega columna centroide para verificar a qué centroide pertenece cada punto
+        centroides = np.array(data.sample(n = k)) #se inicializan los k centroides aleatorios
+        print(centroides)
+        df_centroides = pd.DataFrame(centroides)
+        graficarData(data, df_centroides)
+    else:
+        errorClusters()
 
     #EN EL CORE SE DEBERÍA HACER UNA ITERACIÓN Y ESPERAR EL EVENT DEL BOTON DE AVANZAR UNA ITERACIÓN
     #EN CASO DEL BOTÓN AVANZAR HASTA EL FINAL, EJECUTAR TODO EL ALGORITMO Y RECIÉN AHÍ MOSTRAR
@@ -43,8 +38,19 @@ def importarDataset():
     v.set(csv_file_path)
     df = pd.read_csv(csv_file_path, sep= ';', decimal=",")
     df.plot(kind='scatter', x= 'X', y= 'Y')
-    plt.title('Dataset')
+    plt.title('Dataset sin Aplicar K-Means')
     plt.show()
+
+def errorClusters():
+    new2 = Tk()
+    new2.resizable(0, 0)
+    new2.config(background='grey')
+    new2.title("Error de Clusters")
+    new2.geometry("300x150")
+    error1 = Label(new2, text="Error:",bg='grey',font=("times", 18, "bold"),justify='center')
+    error2 = Label(new2, text="Cantidad de Clusters no Válida.\nIngrese entre 2 y 5 Clusters",bg='grey',font=("times", 14, "bold"),justify='center')
+    error1.place(x=120, y=25)
+    error2.place(x=25, y=75)
 
 
 if __name__=='__main__':
@@ -64,7 +70,7 @@ if __name__=='__main__':
     buttonImportar = Button(new, text='Buscar Dataset \n y Mostrar',fg='White',bg='#001A33', font=("times", 12, "bold"), justify='center', command=importarDataset)
     buttonCerrar = Button(new, text='Cerrar', fg='White',bg='#2F2F2F', font=("times", 12, "bold"), command=new.destroy)
 
-    #adjusting widgets in position
+    #se ajustan los widgets en su posición 
     lbDataset.place(x=20, y=50)
     entryDataset.place(x=65,y=90, width=225,height=25)
     buttonImportar.place(x=110,y=130, height=50)
